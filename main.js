06ea7ef9255eae270e6f8a7ba8df6a22dc1e4ba3
Ali A. Dhillon <ali.dhillon102@gmail.com>
2019-09-25 14:43:10 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        product: 'Socks',
+        des: 'These are good socks for winter',
+        image: './assets/socks.png',
+        click: 'http://127.0.0.1:5500/index.html',
+        inven: 0,
+        onSale: true
+    }
+});
